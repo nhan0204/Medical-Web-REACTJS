de678fb88a62343354614f552a365114d69d7f06
sh1ba <you@example.com>
2024-06-22 11:50:09 +0700

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-<a href="https://serpent.academy">by Serpent Academy</a>
 <p>NFT mint with Metaplex, React, Solana and Typescript</p>
-<img src="/screenshot.png">
+<img src="/screenshot.png"># hack16-solana-medita
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(
-
-
     <React.StrictMode>
         <App />
     </React.StrictMode>,
