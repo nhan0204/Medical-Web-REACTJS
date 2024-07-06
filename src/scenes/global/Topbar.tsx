9a5f4893d79a17ecc5066271e0e4704eb7657e66
TinTin1801 <phuchoang1801@gmail.com>
2024-07-06 10:31:53 +0700

    phucngo

--- src/scenes/global/Topbar.tsx
+++ src/scenes/global/Topbar.tsx
@@ -4,16 +4,38 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { Box, IconButton, useTheme } from "@mui/material";
 import InputBase from "@mui/material/InputBase";
 import { useContext } from "react";
 import SolanaWallet from "../../solana/SolanaWallet";
 import { ColorModeContext, tokens } from "../../theme";
+import { 
+  Box, 
+  IconButton, 
+  useTheme,
+  Menu
+}
+from "@mui/material";
+import { MenuItem } from "react-pro-sidebar";
+
+import { useState } from "react";
+
+require("../../App.css");
+
+
 
 const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const ColorMode = useContext(ColorModeContext);
+  
+  const [anchorEl, setEnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setEnchorEl(event.currentTarget)
+  } 
+  const handelClose = () => {
+    setEnchorEl(null)
+  }
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
@@ -31,7 +53,8 @@ const Topbar = () => {
           <SearchIcon />
         </IconButton>
       </Box>
-      <Box display="flex">
+
+      <Box id="topbar-buttons" display="flex">
         <SolanaWallet></SolanaWallet>
         <IconButton onClick={ColorMode.toggleColorMode}>
           {theme.palette.mode === "light" ? (
@@ -40,9 +63,16 @@ const Topbar = () => {
             <LightModeOutlinedIcon />
           )}
         </IconButton>
-        <IconButton>
+
+        <IconButton id="notification-button" 
+        onClick={handleClick} 
+        aria-controls={open ? 'notification-menu' : undefined} 
+        aria-haspopup = 'true'
+        aria-expanded ={open ? 'true' : undefined}
+        >
           <NotificationsOutlinedIcon />
         </IconButton>
+
         <IconButton>
           <SettingsOutlinedIcon />
         </IconButton>
@@ -50,6 +80,24 @@ const Topbar = () => {
           <PersonOutlinedIcon />
         </IconButton>
       </Box>
+
+      <Menu id="notification-menu" 
+      className="notiMenu"
+      anchorEl={anchorEl} 
+      open = {open} 
+      MenuListProps={{
+        'aria-labelledby' : 'notification-button'
+      }}
+      onClose={handelClose}
+      >
+          {/* <MenuItem onClick={handelClose}> Blog </MenuItem> */}
+          {/* <MenuItem onClick={handelClose}> Poscast </MenuItem> */}
+          <div className="notification_dd">
+
+
+          </div>
+
+      </Menu>
     </Box>
   );
 };
